280cfc455c520fa19fefe0bd64d0925482627b20
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-15 16:49:07 +0300

    Scripted auto-commit on change (2020-10-15 16:49:07) by gitwatch.sh

--- Lab2/src/main/java/KeyCompare.java
+++ Lab2/src/main/java/KeyCompare.java
@@ -1,4 +1,22 @@
-import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.io.WritableComparable;
 
-public class KeyCompare extends WritableComparator {
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class KeyCompare implements WritableComparable<> {
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+
+    }
 }
