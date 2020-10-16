f09f9de34c1f6b8b28734dda1dac254e562a4d43
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-17 00:57:07 +0300

    Scripted auto-commit on change (2020-10-17 00:57:07) by gitwatch.sh

--- Lab2/src/main/java/Main.java
+++ Lab2/src/main/java/Main.java
@@ -13,6 +13,7 @@ public class Main {
             FileInputFormat.addInputPath(job, new Path(args[0]));
             FileOutputFormat.setOutputPath(job, new Path(args[1]));
             job.setMapperClass(AirportsMapper.class);
+            job.setPartitionerClass(AirportPartitioner.class);
             job.setOutputKeyClass(Text.class);
             job.setOutputValueClass(Text.class);
             job.setNumReduceTasks(2);
