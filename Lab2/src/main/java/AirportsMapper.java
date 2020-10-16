1c011e31f2520dc75c82efe6b356137024ceb241
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-16 21:33:01 +0300

    Scripted auto-commit on change (2020-10-16 21:33:01) by gitwatch.sh

--- Lab2/src/main/java/AirportsMapper.java
+++ Lab2/src/main/java/AirportsMapper.java
@@ -2,6 +2,11 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportsMapper extends Mapper<LongWritable, Text,Data,Text> {
-    
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
