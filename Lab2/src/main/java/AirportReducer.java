4a35e6a0801023995297f6d24973c57cd1ada5e0
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-17 01:42:16 +0300

    Scripted auto-commit on change (2020-10-17 01:42:16) by gitwatch.sh

--- Lab2/src/main/java/AirportReducer.java
+++ Lab2/src/main/java/AirportReducer.java
@@ -8,7 +8,7 @@ public class AirportReducer extends Reducer<Data, Text, IntWritable,Text> {
     @Override
     protected void reduce(Data key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         for (Text t:values){
-            context.write(t,);
+            context.write();
         }
     }
 }
