81bc69d677b7f51b859f46a807debbd385c9f9f0
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-16 21:40:43 +0300

    Scripted auto-commit on change (2020-10-16 21:40:43) by gitwatch.sh

--- Lab2/src/main/java/GroupComparator.java
+++ Lab2/src/main/java/GroupComparator.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class GroupComparator {
+import org.apache.hadoop.io.WritableComparator;
+
+public class GroupComparator extends WritableComparator {
+    @Override
+    public int compare(Object a, Object b) {
+        return ((Data)a).compareTo((Data)b);
+    }
 }
