f5134151c6e6ae0f9a146d27c82a09dc6db10f2a
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-16 21:52:06 +0300

    Scripted auto-commit on change (2020-10-16 21:52:06) by gitwatch.sh

--- Lab2/src/main/java/Main.java
+++ Lab2/src/main/java/Main.java
@@ -1,4 +1,6 @@
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 
 public class Main {
     public static void main(String[] args){
@@ -6,6 +8,9 @@ public class Main {
             Job job=Job.getInstance();
             job.setJarByClass(Main.class);
             job.setJobName("Lab2");
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileInputFormat.addInputPath(job, new Path(args[1]));
+            job.setMapperClass(AirportsMapper.class);
             
         }catch (Exception e){
             e.printStackTrace();
