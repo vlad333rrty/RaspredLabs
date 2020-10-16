ae5236b122f9c5f1569713b0d124909c618b4fb6
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-16 21:11:53 +0300

    Scripted auto-commit on change (2020-10-16 21:11:53) by gitwatch.sh

--- Lab2/src/main/java/AirportPartitioner.java
+++ Lab2/src/main/java/AirportPartitioner.java
@@ -1,4 +1,8 @@
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class AirportPartitioner extends Partitioner {
+    @Override
+    public int getPartition(Object o, Object o2, int numPartitions) {
+        return 0;
+    }
 }
