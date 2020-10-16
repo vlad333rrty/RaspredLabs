a7d174f12959b4a9ae76243b3ae95ae91beb44f9
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-16 21:08:05 +0300

    Scripted auto-commit on change (2020-10-16 21:08:05) by gitwatch.sh

--- Lab2/src/main/java/Main.java
+++ Lab2/src/main/java/Main.java
@@ -1,5 +1,13 @@
+import org.apache.hadoop.mapreduce.Job;
+
 public class Main {
     public static void main(String[] args){
-        
+        try{
+            Job job=Job.getInstance();
+            
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
