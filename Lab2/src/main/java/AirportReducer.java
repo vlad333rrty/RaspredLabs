7ec2bda7053d85ee38159f1b435894b029b0a5f8
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-17 01:33:01 +0300

    Scripted auto-commit on change (2020-10-17 01:33:01) by gitwatch.sh

--- Lab2/src/main/java/AirportReducer.java
+++ Lab2/src/main/java/AirportReducer.java
@@ -1,4 +1,26 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.w3c.dom.Text;
 
-public class AirportReducer extends Reducer<Data,> {
+import java.io.IOException;
+import java.util.Iterator;
+
+public class AirportReducer implements Reducer<Data, Text, IntWritable, Text> {
+    @Override
+    public void reduce(Data key, Iterator<Text> values, OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
+
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+
+    @Override
+    public void configure(JobConf job) {
+
+    }
 }
