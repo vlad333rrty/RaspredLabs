37c5269cebc772c024bcf306875c7e23f5713463
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-18 18:36:31 +0300

    Scripted auto-commit on change (2020-10-18 18:36:31) by gitwatch.sh

--- Lab2/src/main/java/Main.java
+++ Lab2/src/main/java/Main.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Main {
     public static void main(String[] args){
         if (args.length!=3){
-            System.err.println("Usage: <input path> <output path>");
+            System.err.println("Usage: <input path> <input path> <output path>");
             System.exit(-1);
         }
         try{
