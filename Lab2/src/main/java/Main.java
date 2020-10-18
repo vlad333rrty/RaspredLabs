3875448169a5262e680b08a1eec06ae919ef5cb0
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-18 19:02:34 +0300

    Scripted auto-commit on change (2020-10-18 19:02:34) by gitwatch.sh

--- Lab2/src/main/java/Main.java
+++ Lab2/src/main/java/Main.java
@@ -1,7 +1,6 @@
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
