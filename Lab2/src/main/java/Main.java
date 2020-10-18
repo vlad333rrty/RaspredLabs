8f961bab7e9428c89722327b0262fafa9f195257
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-18 19:06:20 +0300

    Scripted auto-commit on change (2020-10-18 19:06:20) by gitwatch.sh

--- Lab2/src/main/java/Main.java
+++ Lab2/src/main/java/Main.java
@@ -1,15 +1,11 @@
-import org.apache.hadoop.fs.*;
-import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.Progressable;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URI;
 
 public class Main {
     public static void main(String[] args){
@@ -25,62 +21,7 @@ public class Main {
             MultipleInputs.addInputPath(job,new Path(args[0]), TextInputFormat.class,AirportsMapper.class);
 
 
-            FileSystem fileSystem=new FileSystem() {
-                @Override
-                public URI getUri() {
-                    return null;
-                }
-
-                @Override
-                public FSDataInputStream open(Path f, int bufferSize) throws IOException {
-                    return null;
-                }
-
-                @Override
-                public FSDataOutputStream create(Path f, FsPermission permission, boolean overwrite, int bufferSize, short replication, long blockSize, Progressable progress) throws IOException {
-                    return null;
-                }
-
-                @Override
-                public FSDataOutputStream append(Path f, int bufferSize, Progressable progress) throws IOException {
-                    return null;
-                }
-
-                @Override
-                public boolean rename(Path src, Path dst) throws IOException {
-                    return false;
-                }
-
-                @Override
-                public boolean delete(Path f, boolean recursive) throws IOException {
-                    return false;
-                }
-
-                @Override
-                public FileStatus[] listStatus(Path f) throws FileNotFoundException, IOException {
-                    return new FileStatus[0];
-                }
-
-                @Override
-                public void setWorkingDirectory(Path new_dir) {
-
-                }
-
-                @Override
-                public Path getWorkingDirectory() {
-                    return null;
-                }
-
-                @Override
-                public boolean mkdirs(Path f, FsPermission permission) throws IOException {
-                    return false;
-                }
-
-                @Override
-                public FileStatus getFileStatus(Path f) throws IOException {
-                    return null;
-                }
-            }
+            FileSystem fileSystem=FileSystem.get(new Configuration());
 
             //FileInputFormat.addInputPath(job, new Path(args[0]));
             FileOutputFormat.setOutputPath(job, new Path(args[2]));
