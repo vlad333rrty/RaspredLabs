5cf57c60121b42468182f50e21f7bf7c6b281a45
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-19 22:10:29 +0300

    Scripted auto-commit on change (2020-10-19 22:10:29) by gitwatch.sh

---
+++ Lab2/script.sh
@@ -0,0 +1,13 @@
+mvn package
+
+hadoop fs -rm airports.csv
+hadoop fs -rm data.csv
+hadoop fs -rmdir --ignore-fail-on-non-empty output
+
+hadoop fs -copyFromLocal airports.csv
+hadoop fs -copyFromLocal data.csv
+
+export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
+
+hadoop Main data.csv airports.csv output
+hadoop fs -copyToLocal output
