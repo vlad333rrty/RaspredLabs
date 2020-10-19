aeabbed728799a657675ddf7080713112f85822b
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-19 22:58:49 +0300

    Scripted auto-commit on change (2020-10-19 22:58:49) by gitwatch.sh

--- Lab2/src/main/java/AirportsMapper.java
+++ Lab2/src/main/java/AirportsMapper.java
@@ -8,6 +8,9 @@ public class AirportsMapper extends Mapper<LongWritable,Text,Data,Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] data=value.toString().split(",");
+        for (int i=0;i<data.length;i++){
+            
+        }
         if (key.get()>0) {
             int id=Integer.parseInt(data[0]);
             context.write(new Data(id,true),new Text(data[1]));
