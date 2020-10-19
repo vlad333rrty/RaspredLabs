1e95b8a4a10a4f3ee2f8ca6d507b2b23fc8e2e20
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-19 22:47:50 +0300

    Scripted auto-commit on change (2020-10-19 22:47:50) by gitwatch.sh

--- Lab2/src/main/java/DataMapper.java
+++ Lab2/src/main/java/DataMapper.java
@@ -9,6 +9,8 @@ public class DataMapper extends Mapper<LongWritable, Text,Data,Text> {
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] data=value.toString().split(",");
 
-        context.write(new Data(10,true),new Text(data[0]));
+        if (key.get()>0){
+            context.write(new Data(10,true),new Text(data[0]));
+        }
     }
 }
