efdb19d008a183355a8b120c4163e18877b54ec6
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-20 15:11:14 +0300

    Scripted auto-commit on change (2020-10-20 15:11:14) by gitwatch.sh

--- Lab2/src/main/java/ru/bmstu/iu9/lab2/core/GroupComparator.java
+++ Lab2/src/main/java/ru/bmstu/iu9/lab2/core/GroupComparator.java
@@ -11,6 +11,6 @@ public class GroupComparator extends WritableComparator {
 
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
-        return ((Key)a).getId() - ((Key)b).getId();
+        return a.compareTo(b);
     }
 }
