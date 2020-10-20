2f5e3b5002f0fabee8d154d4d2115f064fe87b09
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-20 13:43:44 +0300

    Scripted auto-commit on change (2020-10-20 13:43:44) by gitwatch.sh

---
+++ Lab2/src/main/java/DataMapper.java
@@ -0,0 +1,16 @@
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class DataMapper extends Mapper<LongWritable,Text,Key, Text>{
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] data=value.toString().split(",");
+
+        if (key.get()>0){
+
+        }
+    }
+}
