fc09ef6ed1ae54a778a3b3f3f9f953ddb8547094
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-20 14:46:04 +0300

    Scripted auto-commit on change (2020-10-20 14:46:04) by gitwatch.sh

--- Lab2/src/main/java/ResultReducer.java
+++ Lab2/src/main/java/ResultReducer.java
@@ -3,10 +3,27 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class ResultReducer extends Reducer<Key, Text, IntWritable,Text>{
     @Override
     protected void reduce(Key key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        super.reduce(key, values, context);
+        Iterator<Text> it=values.iterator();
+
+        double maxDelay=Double.MIN_VALUE;
+        double minDelay=Double.MAX_VALUE;
+        double averageDelayTime=0;
+        int i=0;
+
+        while (it.hasNext()){
+            double t=Double.parseDouble(it.next().toString());
+            averageDelayTime+=t;
+            maxDelay=Math.max(t,maxDelay);
+            minDelay=Math.min(t,minDelay);
+            i++;
+        }
+
+        averageDelayTime/=i;
+        context.write(new IntWritable());
     }
 }
