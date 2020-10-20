92effbcb3c4282922e59beb2e4b6a5175a733755
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-20 13:25:07 +0300

    Scripted auto-commit on change (2020-10-20 13:25:07) by gitwatch.sh

--- Lab2/src/main/java/Key.java
+++ Lab2/src/main/java/Key.java
@@ -1,6 +1,20 @@
-public class Key{
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class Key implements WritableComparable {
     private int id;
     private Type type;
+
+    public Key(){}
+
+    public Key(int id,Type type){
+        this.id=id;
+        this.type=type;
+    }
+
 
     public int getId() {
         return id;
@@ -16,6 +30,21 @@ public class Key{
 
     public void setType(Type type) {
         this.type = type;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+
     }
 
     public enum Type{
