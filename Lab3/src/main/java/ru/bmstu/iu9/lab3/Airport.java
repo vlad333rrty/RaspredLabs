59c2758b4e14a7d51357060bcc72fbabc7ede8c9
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-27 23:23:02 +0300

    Scripted auto-commit on change (2020-10-27 23:23:02) by gitwatch.sh

--- Lab3/src/main/java/ru/bmstu/iu9/lab3/Airport.java
+++ Lab3/src/main/java/ru/bmstu/iu9/lab3/Airport.java
@@ -3,4 +3,18 @@ package ru.bmstu.iu9.lab3;
 import scala.Serializable;
 
 public class Airport implements Serializable {
+    private final String name;
+    private final long id;
+    public Airport(long id,String name){
+        this.id=id;
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
--- Lab3/src/main/java/ru/bmstu/iu9/lab3/Flight.java
+++ Lab3/src/main/java/ru/bmstu/iu9/lab3/Flight.java
@@ -1,4 +1,13 @@
 package ru.bmstu.iu9.lab3;
 
-public class Flight {
+import scala.Serializable;
+
+public class Flight implements Serializable {
+    private final Airport origin,destination;
+
+    public Flight(Airport origin,Airport destination){
+        this.origin=origin;
+        this.destination=destination;
+        
+    }
 }
