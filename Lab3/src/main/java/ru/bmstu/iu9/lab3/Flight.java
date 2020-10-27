3674541303cb8e090b5ff3e4cae3a5bfb73ffcdf
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-27 23:26:05 +0300

    Scripted auto-commit on change (2020-10-27 23:26:05) by gitwatch.sh

--- Lab3/src/main/java/ru/bmstu/iu9/lab3/Flight.java
+++ Lab3/src/main/java/ru/bmstu/iu9/lab3/Flight.java
@@ -3,6 +3,8 @@ package ru.bmstu.iu9.lab3;
 import scala.Serializable;
 
 public class Flight implements Serializable {
+    
+
     private final Airport origin,destination;
 
     public Flight(Airport origin,Airport destination){
