8d231ed57cfb449601d8c5c7233b8c8259395f7a
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-27 17:30:39 +0300

    Scripted auto-commit on change (2020-10-27 17:30:39) by gitwatch.sh

--- Lab3/script.sh
+++ Lab3/script.sh
@@ -8,3 +8,5 @@ hadoop fs -copyFromLocal airports.csv
 hadoop fs -copyFromLocal data.csv
 
 ~/spark-2.4.7-bin-hadoop2.7/bin/spark-submit --class Main --master yarn-client --num-executors 3 target/spark-examples-1.0-SNAPSHOT.jar
+
+hadoop fs -copyToLocal result
