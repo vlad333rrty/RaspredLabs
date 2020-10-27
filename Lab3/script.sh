73d21ccaceef2b37a32b5152434ab6bb4ab77d4a
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-27 15:46:47 +0300

    Scripted auto-commit on change (2020-10-27 15:46:47) by gitwatch.sh

--- Lab3/script.sh
+++ Lab3/script.sh
@@ -1,3 +1,3 @@
 mvn package
 
-spark-submit --class ru.bmstu.iu9.lab3.Main --master yarn-client --num-executors 3 target/
+spark-submit --class ru.bmstu.iu9.lab3.Main --master yarn-client --num-executors 3 target/spark-examples-1.0-SNAPSHOT.jar
