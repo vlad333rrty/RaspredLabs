c5a92cceecb3fef01297ae515c359c995782d45e
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-27 15:36:55 +0300

    Scripted auto-commit on change (2020-10-27 15:36:55) by gitwatch.sh

--- Lab3/src/main/java/Main.java
+++ Lab3/src/main/java/Main.java
@@ -2,6 +2,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.Arrays;
 
@@ -11,6 +12,6 @@ public class Main {
         JavaSparkContext sc=new JavaSparkContext(conf);
         JavaRDD<String> file=sc.textFile("airports.csv");
         JavaPairRDD<String,String> pair=file.flatMap((s)-> Arrays.asList(s.split(",")).iterator())
-                .mapToPair(s->)
+                .mapToPair(s->new Tuple2<>())
     }
 }
