56364c50b525659424ef67c07924f64983b90842
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-27 23:27:44 +0300

    Scripted auto-commit on change (2020-10-27 23:27:44) by gitwatch.sh

--- Lab3/src/main/java/ru/bmstu/iu9/lab3/Flight.java
+++ Lab3/src/main/java/ru/bmstu/iu9/lab3/Flight.java
@@ -3,10 +3,8 @@ package ru.bmstu.iu9.lab3;
 import scala.Serializable;
 
 public class Flight implements Serializable {
-    private static final int CANCELLED=19;
-    private static final int DELAY_TIME=18;
-
     private final Airport origin,destination;
+    private final boolean isCancelled;
 
     public Flight(Airport origin,Airport destination){
         this.origin=origin;
