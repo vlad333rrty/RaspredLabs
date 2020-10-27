d90b9d725d53d24433c19a9f7f14b09d3b77502c
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-27 15:30:50 +0300

    Scripted auto-commit on change (2020-10-27 15:30:50) by gitwatch.sh

--- Lab3/src/main/java/Main.java
+++ Lab3/src/main/java/Main.java
@@ -4,6 +4,6 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class Main {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("Lab3");
-        JavaSparkContext sc=
+        JavaSparkContext sc=new JavaSparkContext()
     }
 }
