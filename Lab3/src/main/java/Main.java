3379905ea0addff7aa556d45e4f1f514029a9271
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-27 15:34:20 +0300

    Scripted auto-commit on change (2020-10-27 15:34:20) by gitwatch.sh

--- Lab3/src/main/java/Main.java
+++ Lab3/src/main/java/Main.java
@@ -1,11 +1,12 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class Main {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("Lab3");
         JavaSparkContext sc=new JavaSparkContext(conf);
-        sc.textFile("airports.csv");
+        JavaRDD<String> file=sc.textFile("airports.csv");
         
     }
 }
