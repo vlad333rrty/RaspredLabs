3b22e2ee0f4e1b5f73c6eb2a5bdba797cc70d361
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-28 22:46:04 +0300

    Scripted auto-commit on change (2020-10-28 22:46:04) by gitwatch.sh

--- Lab3/src/main/java/ru/bmstu/iu9/lab3/Application.java
+++ Lab3/src/main/java/ru/bmstu/iu9/lab3/Application.java
@@ -1,2 +1,40 @@
-package ru.bmstu.iu9.lab3;public class Application {
+package ru.bmstu.iu9.lab3;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
+import ru.bmstu.iu9.lab3.core.Airport;
+import ru.bmstu.iu9.lab3.core.Flight;
+import ru.bmstu.iu9.lab3.core.Utils;
+
+import java.util.Map;
+
+public class Application {
+    private static final String RESULT_FILE_NAME="result";
+    private static final String APP_NAME="Lab3";
+    private static final String AIRPORTS_DATA_FILE_NAME="airports.csv";
+    private static final String FLIGHTS_DATA_FILE_NAME="flights.csv";
+
+    public void run(){
+        SparkConf conf = new SparkConf().setAppName(APP_NAME);
+        JavaSparkContext sc=new JavaSparkContext(conf);
+        JavaRDD<String> airports= Utils.getPreparedData(sc.textFile(AIRPORTS_DATA_FILE_NAME));
+        JavaRDD<String> flights=Utils.getPreparedData(sc.textFile(FLIGHTS_DATA_FILE_NAME));
+
+        JavaRDD<Airport> airportsRDD= Utils.getAirportsRDD(airports);
+        JavaRDD<Flight> flightsRDD=Utils.getFlightsRDD(flights);
+
+        JavaPairRDD<String,Airport> airportsPair=Utils.getPairRDD(airports);
+
+        final Broadcast<Map<String,Airport>> broadcast=sc.broadcast(airportsPair.collectAsMap());
+
+
+        airportsPair.saveAsTextFile(RESULT_FILE_NAME);
+    }
+
+    private class Data{
+        
+    }
 }
--- Lab3/src/main/java/ru/bmstu/iu9/lab3/Main.java
+++ Lab3/src/main/java/ru/bmstu/iu9/lab3/Main.java
@@ -1,40 +1,7 @@
 package ru.bmstu.iu9.lab3;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.broadcast.Broadcast;
-import ru.bmstu.iu9.lab3.core.Airport;
-import ru.bmstu.iu9.lab3.core.Flight;
-import ru.bmstu.iu9.lab3.core.Utils;
-import scala.Tuple2;
-
-import java.util.Collections;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class Main {
-    private static final String RESULT_FILE_NAME="result";
-    private static final String APP_NAME="Lab3";
-    private static final String AIRPORTS_DATA_FILE_NAME="airports.csv";
-    private static final String FLIGHTS_DATA_FILE_NAME="flights.csv";
-
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName(APP_NAME);
-        JavaSparkContext sc=new JavaSparkContext(conf);
-        JavaRDD<String> airports=Utils.getPreparedData(sc.textFile(AIRPORTS_DATA_FILE_NAME));
-        JavaRDD<String> flights=Utils.getPreparedData(sc.textFile(FLIGHTS_DATA_FILE_NAME));
-
-        JavaRDD<Airport> airportsRDD= Utils.getAirportsRDD(airports);
-        JavaRDD<Flight> flightsRDD=Utils.getFlightsRDD(flights);
-
-        JavaPairRDD<String,Airport> airportsPair=Utils.getPairRDD(airports);
-
-        final Broadcast<Map<String,Airport>> broadcast=sc.broadcast(airportsPair.collectAsMap());
-
-        JavaPairRDD<Tuple2<Airport,Airport>,>
-
-        airportsPair.saveAsTextFile(RESULT_FILE_NAME);
+       new Application().run();
     }
 }
