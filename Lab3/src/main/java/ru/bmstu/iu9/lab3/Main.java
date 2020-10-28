4a3e1d5819d133948e85cdb8128bb75d3e73a099
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-28 15:14:21 +0300

    Scripted auto-commit on change (2020-10-28 15:14:21) by gitwatch.sh

--- Lab3/src/main/java/ru/bmstu/iu9/lab3/Main.java
+++ Lab3/src/main/java/ru/bmstu/iu9/lab3/Main.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import ru.bmstu.iu9.lab3.core.Airport;
 import scala.Tuple2;
 
 import java.util.Arrays;
@@ -12,16 +13,21 @@ public class Main {
     private static final String RESULT_FILE_NAME="result";
     private static final String APP_NAME="Lab3";
     private static final String AIRPORTS_DATA_FILE_NAME="airports.csv";
-    private static final String FLIGHT_DATA_FILE_NAME="flights.csv";
+    private static final String FLIGHTS_DATA_FILE_NAME="flights.csv";
+    private static final String DELIMITER=",";
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName(APP_NAME);
         JavaSparkContext sc=new JavaSparkContext(conf);
         JavaRDD<String> airports=sc.textFile(AIRPORTS_DATA_FILE_NAME);
-        JavaRDD<String> flights=sc.textFile(FLIGHT_DATA_FILE_NAME);
+        JavaRDD<String> flights=sc.textFile(FLIGHTS_DATA_FILE_NAME);
 
-        JavaPairRDD<Integer,String> air=airports.map(s->s.split(","))
+        JavaPairRDD<Integer,String> air=airports.map(s->s.split(DELIMITER))
                 .mapToPair(s->new Tuple2<>(Integer.parseInt(s[0].substring(1,s[0].length()-1)),s[1].substring(1)));
-        air.saveAsTextFile(RESULT_FILE_NAME);
+
+        JavaRDD<Airport> rdd=airports.map(s->s.split(DELIMITER))
+                .map(s->new Airport(Integer.parseInt(s[0].substring(1,s[0].length()-1)),s[1].substring(1)));
+
+
     }
 }
