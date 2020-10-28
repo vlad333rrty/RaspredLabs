d79cdd5fd16600523b75c8611c4f026b057cbb20
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-28 21:26:05 +0300

    Scripted auto-commit on change (2020-10-28 21:26:05) by gitwatch.sh

--- Lab3/src/main/java/ru/bmstu/iu9/lab3/Main.java
+++ Lab3/src/main/java/ru/bmstu/iu9/lab3/Main.java
@@ -28,6 +28,6 @@ public class Main {
         JavaRDD<Airport> airportsRDD= Utils.getAirportsRDD(airports);
         JavaRDD<Flight> flightsRDD=Utils.getFlightsRDD(flights);
 
-        final Broadcast<>
+        final Broadcast<Map<String>>
     }
 }
