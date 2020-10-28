13a1c747b7baea1858e88f9dfec59ca435276208
vlad333rrty <vlad333rrty90@mail.ru>
2020-10-28 15:42:00 +0300

    Scripted auto-commit on change (2020-10-28 15:42:00) by gitwatch.sh

--- Lab3/src/main/java/ru/bmstu/iu9/lab3/Main.java
+++ Lab3/src/main/java/ru/bmstu/iu9/lab3/Main.java
@@ -1,25 +1,16 @@
 package ru.bmstu.iu9.lab3;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import ru.bmstu.iu9.lab3.core.Airport;
-import ru.bmstu.iu9.lab3.core.Flight;
-import scala.Tuple2;
-
-import java.util.Arrays;
+import ru.bmstu.iu9.lab3.core.Utils;
 
 public class Main {
     private static final String RESULT_FILE_NAME="result";
     private static final String APP_NAME="Lab3";
     private static final String AIRPORTS_DATA_FILE_NAME="airports.csv";
     private static final String FLIGHTS_DATA_FILE_NAME="flights.csv";
-    private static final String DELIMITER=",";
-
-    private static final int IS_CANCELLED=19;
-    private static final int DELAY_TIME=18;
-    private static final int AIRPORT_ID=14;
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName(APP_NAME);
@@ -27,10 +18,8 @@ public class Main {
         JavaRDD<String> airports=sc.textFile(AIRPORTS_DATA_FILE_NAME);
         JavaRDD<String> flights=sc.textFile(FLIGHTS_DATA_FILE_NAME);
 
-        JavaRDD<Airport> airportsRdd=airports.map(s->s.split(DELIMITER)).filter(s->s.length>2)
-                .map(s->new Airport(Integer.parseInt(s[0].substring(1,s[0].length()-1)),s[1].substring(1)));
+        JavaRDD<Airport> airportsRDD= Utils.getAirportsRDD(airports);
 
-        JavaRDD<Flight> flightsRdd=flights.map(s->s.split(DELIMITER))
-                .map(s->new Flight())
+
     }
 }
--- Lab3/src/main/java/ru/bmstu/iu9/lab3/core/Utils.java
+++ Lab3/src/main/java/ru/bmstu/iu9/lab3/core/Utils.java
@@ -1,2 +1,25 @@
-package ru.bmstu.iu9.lab3.core;public class FlightUtils {
+package ru.bmstu.iu9.lab3.core;
+
+import org.apache.spark.api.java.JavaRDD;
+
+public class Utils {
+    private static final int IS_CANCELLED=19;
+    private static final int DELAY_TIME=18;
+    private static final int ORIGIN_AIRPORT_ID =14;
+
+    private static final String DELIMITER=",";
+
+    public static JavaRDD<Flight> getFlightsRDD(JavaRDD<String> flights,JavaRDD<Airport> airportsRDD){
+        JavaRDD<Flight> result=flights.map(s->s.split(DELIMITER))
+                .map(s->new Flight(getAirportById(airportsRDD,s[ORIGIN_AIRPORT_ID]),))
+    }
+
+    public static JavaRDD<Airport> getAirportsRDD(JavaRDD<String> airports){
+        return airports.map(s->s.split(DELIMITER)).filter(s->s.length>2)
+                .map(s->new Airport(Integer.parseInt(s[0].substring(1,s[0].length()-1)),s[1].substring(1)));
+    }
+
+    private static Airport getAirportById(JavaRDD<Airport> airportsRDD, String id){
+        return airportsRDD.filter(airport -> airport.getId()==Integer.parseInt(id)).first();
+    }
 }
