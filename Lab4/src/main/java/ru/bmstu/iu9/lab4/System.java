db0884f1b38d0c22592b16a727d5c1af79f487fe
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-05 19:01:58 +0300

    Scripted auto-commit on change (2020-12-05 19:01:58) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/System.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/System.java
@@ -2,7 +2,9 @@ package ru.bmstu.iu9.lab4;
 import akka.actor.ActorSystem;
 
 public class System {
+    private static final ACTOR_NAME=""
+
     public System(){
-        ActorSystem system=ActorSystem.create()
+        ActorSystem system=ActorSystem.create();
     }
 }
