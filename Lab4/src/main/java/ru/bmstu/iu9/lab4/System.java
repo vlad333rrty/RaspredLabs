08182626e52c49890eab4fe1d84c7b1de46bc8c6
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-05 19:19:54 +0300

    Scripted auto-commit on change (2020-12-05 19:19:53) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/System.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/System.java
@@ -17,6 +17,6 @@ public class System {
         ActorSystem system=ActorSystem.create(ACTOR_NAME);
         final Http http =Http.get(system);
         final ActorMaterializer materializer=ActorMaterializer.create(system);
-        MainHttp 
+        MainHttp  
     }
 }
