01421da9a8734fdfe8c6421c2ec6e009d8a1070a
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-05 19:18:46 +0300

    Scripted auto-commit on change (2020-12-05 19:18:46) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/System.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/System.java
@@ -1,11 +1,19 @@
 package ru.bmstu.iu9.lab4;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
+
 
 public class System {
     private static final String ACTOR_NAME="System actor";
 
     public System(){
         ActorSystem system=ActorSystem.create(ACTOR_NAME);
-
+        Http 
     }
 }
