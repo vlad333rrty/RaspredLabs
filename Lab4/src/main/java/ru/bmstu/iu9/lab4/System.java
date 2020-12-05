04f4bca86278b170eb8286858d7412c3cee1b5f6
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-05 19:56:24 +0300

    Scripted auto-commit on change (2020-12-05 19:56:24) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/System.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/System.java
@@ -1,28 +1,13 @@
 package ru.bmstu.iu9.lab4;
-import akka.NotUsed;
 import akka.actor.ActorSystem;
-import akka.http.javadsl.ConnectHttp;
-import akka.http.javadsl.Http;
-import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.marshallers.jackson.Jackson;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
-import akka.http.javadsl.model.headers.Server;
-import akka.http.javadsl.server.Route;
-import akka.stream.ActorMaterializer;
-import akka.stream.javadsl.Flow;
 
-import static akka.http.javadsl.server.Directives.*;
 
 public class System {
-    private static final String ACTOR_NAME="System actor";
+    private static final String ACTOR_NAME="routes";
 
     public System(){
         ActorSystem system=ActorSystem.create(ACTOR_NAME);
-        final Http http =Http.get(system);
-        final ActorMaterializer materializer=ActorMaterializer.create(system);
 
-        
 
     }
 }
