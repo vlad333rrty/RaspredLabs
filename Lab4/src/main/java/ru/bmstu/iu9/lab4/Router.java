49b5442214a56a97f8446ed3012611bc3c5bbc4c
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-06 07:26:18 +0300

    Scripted auto-commit on change (2020-12-06 07:26:18) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/Router.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/Router.java
@@ -1,10 +1,11 @@
 package ru.bmstu.iu9.lab4;
+import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 
 
-public class Router {
+public class Router extends AbstractActor {
     private static final String STORE_ACTOR_NAME = "store";
     private static final String EXECUTE_ACTOR_NAME="execute";
 
@@ -14,5 +15,10 @@ public class Router {
     public Router(ActorSystem system){
         storeActor=system.actorOf(Props.create(StoreActor.class), STORE_ACTOR_NAME);
         executeActor=system.actorOf(Props.create(ExecuteActor.class),EXECUTE_ACTOR_NAME);
+    }
+
+    @Override
+    public Receive createReceive() {
+        return null;
     }
 }
