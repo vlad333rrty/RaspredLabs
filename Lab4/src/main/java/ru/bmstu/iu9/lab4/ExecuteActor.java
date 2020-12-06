dda18902a8be2a40e7890a1436ca10524a7ffb9c
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-06 07:25:50 +0300

    Scripted auto-commit on change (2020-12-06 07:25:50) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/ExecuteActor.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/ExecuteActor.java
@@ -1,5 +1,11 @@
 package ru.bmstu.iu9.lab4;
 
-public class ExecuteActor {
+import akka.actor.AbstractActor;
 
+public class ExecuteActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
