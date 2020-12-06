9c28e9abbe800f40367d51c9df7084ad0ebcfe03
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-06 10:40:56 +0300

    Scripted auto-commit on change (2020-12-06 10:40:56) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/ExecuteActor.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/ExecuteActor.java
@@ -15,32 +15,11 @@ public class ExecuteActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(Request.class,request -> {
-
+                    for (Request.Test test:request.getTests()){
+                        
+                    }
                 })
                 .build();
-    }
-
-    private TestResult executeAndTestJSCode(String code,String functionName,int param,int expectedResult) {
-        TestResultStatus status;
-        String description;
-        try{
-            ScriptEngine engine = new ScriptEngineManager().getEngineByName(ENGINE_NAME);
-            engine.eval(code);
-            Invocable invocable = (Invocable) engine;
-            int result=(int)invocable.invokeFunction(functionName,param);
-            if (result==expectedResult){
-                status=TestResultStatus.SUCCEEDED;
-                description="Test finished successfully";
-            }else{
-                status=TestResultStatus.FAILED;
-                description="Status: FAILED. Reason: Wrong result";
-            }
-            return new TestResult(status,description);
-        }catch (Exception e){
-            status=TestResultStatus.FAILED;
-            description="An error occurred while testing";
-            return new TestResult(status,description);
-        }
     }
 
     private int executeJSCode(String code,String functionName,int param) throws ScriptException, NoSuchMethodException {
@@ -50,11 +29,25 @@ public class ExecuteActor extends AbstractActor {
         return (int) invocable.invokeFunction(functionName, param);
     }
 
-    private TestResult getTestResult(String code,String functionName,int param,int expectedResult,int result){
-        try {
+    private TestResult getTestResult(int expectedResult,int result){
+        TestResultStatus status;
+        String description;
+        if (result==expectedResult){
+            status=TestResultStatus.SUCCEEDED;
+            description="Test completed successfully";
+        }else{
+            status=TestResultStatus.FAILED;
+            description="Wrong answer";
+        }
+        return new TestResult(status,description);
+    }
+
+    private TestResult getTestResult(String code,String functionName,int param,int expectedResult){
+        try{
             int result=executeJSCode(code,functionName,param);
-        } catch (ScriptException e) {
-            e.printStackTrace();
+            return getTestResult(expectedResult,result);
+        }catch (ScriptException | NoSuchMethodException e){
+            return new TestResult(TestResultStatus.FAILED,String.format("An exception occurred while testing:%s",e.getLocalizedMessage()));
         }
     }
 }
