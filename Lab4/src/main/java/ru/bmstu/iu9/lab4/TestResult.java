8cdb2e58fc21345166f7c914339b293e0c86d5c8
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-06 14:11:52 +0300

    Scripted auto-commit on change (2020-12-06 14:11:52) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/TestResult.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/TestResult.java
@@ -3,8 +3,17 @@ package ru.bmstu.iu9.lab4;
 public class TestResult {
     private TestResultStatus status;
     private String description;
+
     public TestResult(TestResultStatus status,String description){
         this.status=status;
         this.description=description;
+    }
+
+    public TestResultStatus getStatus() {
+        return status;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
