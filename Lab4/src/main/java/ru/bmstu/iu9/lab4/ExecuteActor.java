7a3cb9e5599a0430f704eaa1b35d57c1e50064d0
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-06 09:24:34 +0300

    Scripted auto-commit on change (2020-12-06 09:24:34) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/ExecuteActor.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/ExecuteActor.java
@@ -3,6 +3,8 @@ package ru.bmstu.iu9.lab4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.ScriptEngine;
+
 public class ExecuteActor extends AbstractActor {
 
     @Override
@@ -15,6 +17,6 @@ public class ExecuteActor extends AbstractActor {
     }
 
     private void executeJSCode(String code){
-        
+        ScriptEngine
     }
 }
