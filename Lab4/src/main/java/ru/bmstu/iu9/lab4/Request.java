a5a1c7c9905906d34f6f04d674c5a0eb2cc29b9c
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-06 08:59:39 +0300

    Scripted auto-commit on change (2020-12-06 08:59:39) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/Request.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/Request.java
@@ -2,9 +2,13 @@ package ru.bmstu.iu9.lab4;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
+
 public class Request {
     @JsonProperty
     private int packageId;
-
-    
+    @JsonProperty
+    private String function;
+    @JsonProperty
+    private ArrayList<String> tests;
 }
