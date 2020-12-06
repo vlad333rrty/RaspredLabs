d81b80f31fbd6ee540148093a6b5d3d9bfc2ede0
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-06 09:19:59 +0300

    Scripted auto-commit on change (2020-12-06 09:19:59) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/StoreActor.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/StoreActor.java
@@ -28,7 +28,7 @@ public class StoreActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create().match(Request.class,request->{
-            System.out.println();
+            System.out.println("Test received");
         }).build();
     }
 }
