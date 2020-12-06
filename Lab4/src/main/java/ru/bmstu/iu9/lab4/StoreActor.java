17451391725f3b8fb944d013b2c53799c33b2b61
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-06 07:10:06 +0300

    Scripted auto-commit on change (2020-12-06 07:10:06) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/StoreActor.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/StoreActor.java
@@ -1,7 +1,8 @@
 package ru.bmstu.iu9.lab4;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class StoreActor {
-    private final Map<Integer,Object> IntToTest=
+    private final Map<Integer,Object> IntToTest=new ConcurrentHashMap<>();
 }
