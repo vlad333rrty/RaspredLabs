87dbe5714d36fc630b90fe8cf230cccad578655e
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-06 09:25:58 +0300

    Scripted auto-commit on change (2020-12-06 09:25:58) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/ExecuteActor.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/ExecuteActor.java
@@ -23,6 +23,7 @@ public class ExecuteActor extends AbstractActor {
     private void executeJSCode(String code) throws ScriptException {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName(ENGINE_NAME);
         engine.eval(code);
-        Invocable invocable = (Invocable)
+        Invocable invocable = (Invocable) engine;
+        
     }
 }
