3e0546caf028cf90ae28e82f85f27c15723aa358
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-06 09:01:24 +0300

    Scripted auto-commit on change (2020-12-06 09:01:23) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/Request.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/Request.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 
 public class Request {
+    private static final String JSON_PACKAGE_ID_FIELD="package id";
+
     @JsonProperty
     private int packageId;
     @JsonProperty
