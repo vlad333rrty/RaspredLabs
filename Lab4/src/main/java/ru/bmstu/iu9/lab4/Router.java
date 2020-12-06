ea97321ca6172d370425eb21d21579529c0e54e7
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-06 23:17:06 +0300

    Scripted auto-commit on change (2020-12-06 23:17:06) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/Router.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/Router.java
@@ -3,12 +3,13 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.server.Route;
 import akka.japi.pf.ReceiveBuilder;
 import akka.routing.RoundRobinPool;
 import scala.sys.Prop;
 
 
-public class Router extends AbstractActor {
+public class Router{
     private static final String STORE_ACTOR_NAME = "store";
     private static final String EXECUTE_ACTOR_NAME="execute";
     private static final int POOL_NUMBER=10;
@@ -21,12 +22,7 @@ public class Router extends AbstractActor {
         executeActor=system.actorOf(new RoundRobinPool(POOL_NUMBER).props(Props.create(ExecuteActor.class)),EXECUTE_ACTOR_NAME);
     }
 
-    @Override
-    public Receive createReceive() {
-        return ReceiveBuilder.create()
-                .match(Request.class,request -> {
-                    
-                })
-                .build();
+    public Route getRoute(){
+        return 
     }
 }
