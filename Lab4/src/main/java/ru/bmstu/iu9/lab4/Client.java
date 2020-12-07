5f76fdbca3fd0f6b88c29dea4f9831524fd42dc1
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-07 13:02:46 +0300

    Scripted auto-commit on change (2020-12-07 13:02:46) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
@@ -3,6 +3,8 @@ package ru.bmstu.iu9.lab4;
 
 import sun.net.www.http.HttpClient;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 
@@ -10,7 +12,19 @@ public class Client {
     private static final String URL ="localhost:8080";
     public static void main(String[] args) throws IOException {
         HttpClient client = HttpClient.New(new URL(URL));
-        
+
+    }
+
+    private static String readFile(String fileName){
+        try(BufferedInputStream in=new BufferedInputStream(new FileInputStream(fileName))){
+            int c;
+            StringBuilder builder=new StringBuilder();
+            while ((c=in.read())!=-1){
+                builder.append((char)c);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
