929585166b571369c80001ecc093479ce9d101f8
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-07 12:57:15 +0300

    Scripted auto-commit on change (2020-12-07 12:57:15) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
@@ -6,7 +6,8 @@ import sun.net.www.http.HttpClient;
 
 public class Client {
     private static final String URI="http://";
-    public static void main(String[] args){
-        HttpClient client = HttpClient.New(URI);
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.New()
+    }
 
 }
