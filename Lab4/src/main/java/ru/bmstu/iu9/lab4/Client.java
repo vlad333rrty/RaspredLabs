f9171eda2337f6bd47fee75aa0dfb4493b2227c3
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-07 13:00:02 +0300

    Scripted auto-commit on change (2020-12-07 13:00:02) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
@@ -1,18 +1,15 @@
 package ru.bmstu.iu9.lab4;
 
 
-import akka.http.javadsl.model.HttpRequest;
 import sun.net.www.http.HttpClient;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 
 public class Client {
-    private static final String URI="http://";
+    private static final String URL ="localhost:8080";
     public static void main(String[] args) throws IOException {
-        HttpClient client = HttpClient.New(new URL(URI));
+        HttpClient client = HttpClient.New(new URL(URL));
         
     }
 
