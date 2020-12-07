9f76600ddd54ba81dea6d6e01acfda0bbc67603d
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-07 15:53:18 +0300

    Scripted auto-commit on change (2020-12-07 15:53:18) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/Router.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/Router.java
@@ -31,7 +31,7 @@ public class Router{
 
     public Route createRoute(){
         return get(()-> parameter(GET_PARAMETER, id-> {
-            CompletionStage<Object> res= PatternsCS.ask()
+            CompletionStage<Object> res= PatternsCS.ask(storeActor,)
         }).orElse(post(()-> entity(Jackson.unmarshaller(Request.class), request -> {
             System.out.println(request.getCode());
             return null;
