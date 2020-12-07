c92ce51ec65a09d0f19ba23772b9e491b8a118ab
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-07 12:54:54 +0300

    Scripted auto-commit on change (2020-12-07 12:54:53) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
@@ -1,9 +1,17 @@
 package ru.bmstu.iu9.lab4;
 
 
+import sun.net.www.http.HttpClient;
 
 public class Client {
     public static void main(String[] args){
-
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://openjdk.java.net/"))
+                .build();
+        client.sendAsync(request, BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
     }
 }
