7c725a9cc0d01ca1b5779642cf43cd7e035b3b17
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-07 16:35:52 +0300

    Scripted auto-commit on change (2020-12-07 16:35:52) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
@@ -34,7 +34,7 @@ public class Client {
                 .uri(URI.create(GET_REQUEST))
                 .GET()
                 .build();
-        
+        response=httpClient.send(request,HttpResponse.BodyHandlers.ofString())
 
         System.out.println(request.bodyPublisher());
 
