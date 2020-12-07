ddc2863b702c07c80511d2983c06b7ef19c3dc3e
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-07 12:55:37 +0300

    Scripted auto-commit on change (2020-12-07 12:55:37) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
@@ -4,6 +4,7 @@ package ru.bmstu.iu9.lab4;
 import sun.net.www.http.HttpClient;
 
 public class Client {
+    private static final String URI="http://"
     public static void main(String[] args){
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
