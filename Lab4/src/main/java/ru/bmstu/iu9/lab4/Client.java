ccf89c12272b35674b61d093282df701949bf56e
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-07 13:05:23 +0300

    Scripted auto-commit on change (2020-12-07 13:05:23) by gitwatch.sh

--- Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
+++ Lab4/src/main/java/ru/bmstu/iu9/lab4/Client.java
@@ -8,9 +8,11 @@ import java.net.URL;
 
 public class Client {
     private static final String URL ="localhost:8080";
+    private static final String JSON_FILE="request.json";
     public static void main(String[] args) throws IOException {
         HttpClient client = HttpClient.New(new URL(URL));
-
+        String json=readFile(JSON_FILE);
+        
     }
 
     private static String readFile(String fileName){
