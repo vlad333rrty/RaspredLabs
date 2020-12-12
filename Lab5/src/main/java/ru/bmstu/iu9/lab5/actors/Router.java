5e5dec1efed6e98c4b8e9cc4b9148e922db1c48d
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-12 19:27:42 +0300

    Scripted auto-commit on change (2020-12-12 19:27:42) by gitwatch.sh

--- Lab5/src/main/java/ru/bmstu/iu9/lab5/actors/Router.java
+++ Lab5/src/main/java/ru/bmstu/iu9/lab5/actors/Router.java
@@ -59,7 +59,7 @@ public class Router{
                             Integer.parseInt(request.second()));
                     Future<Object> future=Patterns.ask(storeActor,r,TIMEOUT_MILLIS);
 
-                    Sink.fold(0,(agg,next)->)
+                    Sink<>
 
                     Sink<Pair<String,String>, CompletionStage<Double>> testSink=Flow.
                             <Pair<String,String>>create()
