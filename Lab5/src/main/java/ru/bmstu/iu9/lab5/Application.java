af9abad8c2e595d594533e9f2a48df1afef4733d
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-12 16:48:18 +0300

    Scripted auto-commit on change (2020-12-12 16:48:18) by gitwatch.sh

--- Lab5/src/main/java/ru/bmstu/iu9/lab5/Application.java
+++ Lab5/src/main/java/ru/bmstu/iu9/lab5/Application.java
@@ -1,4 +1,7 @@
 package ru.bmstu.iu9.lab5;
 
 public class Application {
+    public static void main(String[] args) {
+
+    }
 }
--- Lab5/src/main/java/ru/bmstu/iu9/lab5/Server.java
+++ Lab5/src/main/java/ru/bmstu/iu9/lab5/Server.java
@@ -1,2 +1,4 @@
-package ru.bmstu.iu9.lab5;public class Server {
+package ru.bmstu.iu9.lab5;
+
+public class Server {
 }
