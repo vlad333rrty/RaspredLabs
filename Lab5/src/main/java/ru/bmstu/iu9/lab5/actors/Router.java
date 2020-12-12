c8e31e86d79816ca2ba552334cfadd2ee10b9462
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-12 16:50:11 +0300

    Scripted auto-commit on change (2020-12-12 16:50:11) by gitwatch.sh

--- Lab5/src/main/java/ru/bmstu/iu9/lab5/actors/Router.java
+++ Lab5/src/main/java/ru/bmstu/iu9/lab5/actors/Router.java
@@ -1,4 +1,32 @@
 package ru.bmstu.iu9.lab5.actors;
 
-public class Router {
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import akka.routing.RoundRobinPool;
+import scala.concurrent.Future;
+
+import static akka.http.javadsl.server.Directives.*;
+
+public class Router{
+    private static final String STORE_ACTOR_NAME = "store";
+    private static final String EXECUTE_ACTOR_NAME="execute";
+    private static final String GET_PARAMETER="packageId";
+    private static final int POOL_NUMBER=10;
+    private static final int TIMEOUT_MILLIS=1000;
+
+    private final ActorRef storeActor;
+    private final ActorRef executeActor;
+
+    public Router(ActorSystem system){
+       storeActor=null;
+       executeActor=null;
+    }
+
+    public Route createRoute(){
+        return null;
+    }
 }
