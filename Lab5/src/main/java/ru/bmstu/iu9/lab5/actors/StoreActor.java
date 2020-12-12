45ea2b1da389a5c8e25e98fbbf4f5b5b0a539890
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-12 17:07:56 +0300

    Scripted auto-commit on change (2020-12-12 17:07:56) by gitwatch.sh

--- Lab5/src/main/java/ru/bmstu/iu9/lab5/actors/StoreActor.java
+++ Lab5/src/main/java/ru/bmstu/iu9/lab5/actors/StoreActor.java
@@ -3,9 +3,10 @@ package ru.bmstu.iu9.lab5.actors;
 import akka.actor.AbstractActor;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class StoreActor extends AbstractActor {
-    private final Map<String,Integer>    
+    private final Map<String,Integer> URLtoInt=new ConcurrentHashMap<>();
 
 
     @Override
