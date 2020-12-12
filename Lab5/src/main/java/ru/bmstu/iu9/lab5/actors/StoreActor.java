53ed72029f02d8184fd3ae41f193e57bd28697f1
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-12 17:06:41 +0300

    Scripted auto-commit on change (2020-12-12 17:06:41) by gitwatch.sh

--- Lab5/src/main/java/ru/bmstu/iu9/lab5/actors/StoreActor.java
+++ Lab5/src/main/java/ru/bmstu/iu9/lab5/actors/StoreActor.java
@@ -3,11 +3,13 @@ package ru.bmstu.iu9.lab5.actors;
 import akka.actor.AbstractActor;
 
 public class StoreActor extends AbstractActor {
+    private final Map<>
+
 
     @Override
     public Receive createReceive() {
         return null;
     }
 
-    
+
 }
