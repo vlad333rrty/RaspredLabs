c461fa8e05c8c7251cebc34f779f1e0b0e465732
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-19 13:50:09 +0300

    Scripted auto-commit on change (2020-12-19 13:50:09) by gitwatch.sh

--- Lab5/src/main/java/ru/bmstu/iu9/lab5/actors/Router.java
+++ Lab5/src/main/java/ru/bmstu/iu9/lab5/actors/Router.java
@@ -61,7 +61,7 @@ public class Router{
 
                     System.out.println(future.value().get().get());
 
-                    
+                    Flow<>
 
                     Sink<Pair<String,Integer>,CompletionStage<Long>> fold=Sink.fold(0L,
                             (agg,next)-> agg+System.currentTimeMillis()-next.second());
