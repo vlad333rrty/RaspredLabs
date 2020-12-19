97ee586f680a8966ff01d2372edadca6266efb0f
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-19 14:03:08 +0300

    Scripted auto-commit on change (2020-12-19 14:03:08) by gitwatch.sh

--- Lab5/src/main/java/ru/bmstu/iu9/lab5/actors/Router.java
+++ Lab5/src/main/java/ru/bmstu/iu9/lab5/actors/Router.java
@@ -57,6 +57,9 @@ public class Router{
                 })
                 .mapAsync(POOL_NUMBER,request->{
                     Flow<Pair<HttpRequest,Long>,Pair<Try<HttpResponse>,Long>,NotUsed> client=http.superPool(materializer);
+
+                    
+
                     Sink<Pair<HttpRequest,Integer>,CompletionStage<Long>> testSink= Flow
                             .<Pair<HttpRequest, Integer>>create()
                             .mapConcat(pair -> new ArrayList<>(Collections.nCopies(pair.second(),pair)))
