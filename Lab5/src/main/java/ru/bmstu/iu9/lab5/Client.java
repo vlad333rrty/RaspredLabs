f6899ac35493a83e46756cea044b438b51336430
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-19 19:31:57 +0300

    Scripted auto-commit on change (2020-12-19 19:31:57) by gitwatch.sh

--- Lab5/src/main/java/ru/bmstu/iu9/lab5/Client.java
+++ Lab5/src/main/java/ru/bmstu/iu9/lab5/Client.java
@@ -3,10 +3,12 @@ package ru.bmstu.iu9.lab5;
 import akka.http.javadsl.Http;
 
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 
 public class Client {
     public static void main(String[] args) {
         HttpClient client= HttpClient.newHttpClient();
-        
+
+        HttpRequest
     }
 }
