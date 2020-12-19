532e7a41f7079e022916a019196c0361e9b3e448
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-19 19:32:09 +0300

    Scripted auto-commit on change (2020-12-19 19:32:09) by gitwatch.sh

--- Lab5/src/main/java/ru/bmstu/iu9/lab5/Client.java
+++ Lab5/src/main/java/ru/bmstu/iu9/lab5/Client.java
@@ -9,6 +9,7 @@ public class Client {
     public static void main(String[] args) {
         HttpClient client= HttpClient.newHttpClient();
 
-        HttpRequest request=HttpRequest
+        HttpRequest request=HttpRequest.newBuilder()
+                .build();
     }
 }
