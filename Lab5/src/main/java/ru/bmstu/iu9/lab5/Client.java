dc69769d2da9293efd0ca4e9fdfe8f400375050a
vlad333rrty <vlad333rrty90@mail.ru>
2020-12-19 19:33:01 +0300

    Scripted auto-commit on change (2020-12-19 19:33:01) by gitwatch.sh

--- Lab5/src/main/java/ru/bmstu/iu9/lab5/Client.java
+++ Lab5/src/main/java/ru/bmstu/iu9/lab5/Client.java
@@ -8,11 +8,12 @@ import java.net.http.HttpRequest;
 
 public class Client {
     private static final String ADDRESS ="http://127.0.0.1:8080";
+    private static final String GET_REQUEST=ADDRESS+"/?packageId=11";
     public static void main(String[] args) {
         HttpClient client= HttpClient.newHttpClient();
 
         HttpRequest request=HttpRequest.newBuilder()
-                .uri(URI.create())
+                .uri(URI.create(GET_REQUEST))
                 .build();
     }
 }
